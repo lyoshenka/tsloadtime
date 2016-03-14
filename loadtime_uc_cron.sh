7c1a0e6b9f8802d7be637531fab26f300eb61e56
Alex Grintsvayg <alex@grin.io>
2016-03-14 13:03:45 -0400

    put data in dirs, pull data script

--- loadtime_uc_cron.sh
+++ loadtime_uc_cron.sh
@@ -3,5 +3,6 @@
 set -euo pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+HOST=$(hostname)
 
-$DIR/loadtime.sh http://ultimatecentral.com >> $DIR/$(date -u '+%F').csv
+$DIR/loadtime.sh http://ultimatecentral.com >> $DIR/$HOST/$(date -u '+%F').csv
---
+++ pulldata.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPT_DIR="$DIR/lftp-scripts"
+
+#lftp -c "open -u USER,PASSWORD -e 'set ssl:verify-certificate false' HOST; mirror --only-newer --include-glob '*.csv' tsloadtime pingboxlv"
+
+for script in $SCRIPT_DIR/*; do
+  echo "Pulling $(basename $script)"
+  lftp -f "$script"
+done
+
+echo "Done"
